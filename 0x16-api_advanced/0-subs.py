272f242745c44a78072f6472d8f8a00c4b4ae371
Adewale Kujore <Adewalekujore6@gmail.com>
2024-05-07 19:55:06 +0100

    updae

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,14 +9,13 @@ def number_of_subscribers(subreddit):
     """ Returns the number of subscribers """
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {
-        'User-Agent': 'Mozilla/5.0'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+            AppleWebKit/537.36 (KHTML, like Gecko)\
+                Chrome/124.0.0.0 Safari/537.36'
     }
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
-        subscribers = response.json().get('data').get('subscribers')
-        if subscribers is not None:
-            return subscribers
-        return 0
+        return response.json().get('data').get('subscribers')
     return 0
 
 
