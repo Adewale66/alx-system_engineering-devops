1c4ba2befb5c45d173bff9bbcfaf0ae54b17f9e1
Adewale Kujore <Adewalekujore6@gmail.com>
2024-05-07 16:13:18 +0100

    first

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Number of subscribers """
+
+import requests
+import sys
+
+
+def number_of_subscribers(subreddit):
+    """ Returns the number of subscribers """
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {
+        'User-Agent': 'Mozilla/5.0'
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json().get('data').get('subscribers')
+    return 0
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Please pass an argument")
+    else:
+        print(number_of_subscribers(sys.argv[1]))
